3119c3031a9315bd2d1ce8aff0291ee5fe692c5f
arshiha bristy <arshihabristy5@gmail.com>
2022-02-28 23:14:38 +0600

    phone link added

---
+++ app.js
@@ -0,0 +1,13 @@
+const allPhones = () => {
+    const searchValue = document.getElementById('search-box').value;
+
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
+  console.log(url);
+  fetch(url)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+      
+
+
+    console.log(searchValue);
+};
