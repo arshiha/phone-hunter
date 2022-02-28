a5ca76f70e6be9cc304515abc848a70220eb753b
arshiha bristy <arshihabristy5@gmail.com>
2022-03-01 00:51:05 +0600

    phone details added

--- app.js
+++ app.js
@@ -2,12 +2,28 @@ const allPhones = () => {
     const searchValue = document.getElementById('search-box').value;
 
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
-  console.log(url);
+//   console.log(url);
   fetch(url)
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => showPhoneDetails(data.data));
       
-
-
-    console.log(searchValue);
 };
+
+
+const showPhoneDetails = (phones) => {
+    for (const phone of phones) {
+        const parent = document.getElementById('phone-container');
+    const div = document.createElement('div');
+    div.innerHTML = ` <div class="card border p-5">
+    <div class="pic">
+      <img class="w-25" src="" alt="">
+    </div>
+    <h2 class="mx-2">Phone Name: cool</h2>
+    <h5 class="mx-2">Phone Brand Name: </h5>
+    <div class="allButton">
+        <button class="btn btn-primary mx-2 my-2">Details</button>
+    </div>
+</div>`;
+   parent.appendChild(div);
+    }
+}
