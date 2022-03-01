d3076316c7dbbe421e92a4135f376d417ba8853c
arshiha bristy <arshihabristy5@gmail.com>
2022-03-02 01:15:40 +0600

    finishing added

--- app.js
+++ app.js
@@ -1,64 +1,111 @@
 const allPhones = () => {
-  document.getElementById("phone-container").innerHTML = "";
-  document.getElementById("spinner").style.display = "block";
-    const searchValue = document.getElementById('search-box').value;
-
-  const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
-
-//   console.log(url);
-  fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data.data == null)
-    if(data.data == null) {
-      document.getElementById("spinner").style.display = "block";
-    }
-    else {
-      showPhoneDetails(data.data);
-      document.getElementById("spinner").style.display = "none";
-    }
-  });
+  const searchFiled = document.getElementById('search-box')
+  const searchText = searchFiled.value;
+  searchFiled.value = '';
   
-      
+  //console.log(searchText);
+  const url =` https://openapi.programming-hero.com/api/phones?search=${searchText}`
+  fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+          if (data.data.length == 0) {
+              alert('No result found.Please try again')
+          }
+          else {
+            showPhoneDetails(data.data.slice(0, 20)); 
+          }
+          
+      });
 };
+
 const showPhoneDetails = (phones) => {
     for (const phone of phones) {
         const parent = document.getElementById('phone-container');
     const div = document.createElement('div');
-    div.innerHTML = ` <div class="card border p-5 bg-dark w-75">
-    <div class="pic">
-      <img class="w-25" src="${phone.image}" alt="">
+    div.classList.add("col")
+    div.innerHTML = `
+    <div class="card h-100 shadow-lg p-2">
+        <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
+         <div class="card-body">
+         <h5 class="card-title"> Brand: ${phone.brand}</h5>
+         <h6 class="card-title text-warning">Phone Name: ${phone.phone_name}</h6>
+         <p class="card-text"></p>
+          <button onClick="moreDetails('${phone.slug}')" class="btn btn-primary text-white">Details</button>
+      </div>
     </div>
-    <h2 class="mx-2 text-white">Phone Name: ${phone.slug}</h2>
-    <h5 class="mx-2 text-warning">Phone Brand Name: ${phone.brand}</h5>
-    <div class="allButton">
-        <button onclick="details('${phone.slug}')" class="btn btn-primary mx-2 my-2">Explore</button>
-
-    </div>
-</div>`;
+    `;
 
    parent.appendChild(div);
-   console.log(phone);
+  //  console.log(phone);
     }
 }
 
-const details = (id) => {
+const moreDetails = (id) => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
     fetch(url)
     .then((response) => response.json())
     .then((data) => setDetails(data.data));
+    
 };
 
-const setDetails = (info) => {
-  document.getElementById("details-container").innerHTML = `
-  <div>
-  <img class="mx-5 mt-5" src="${info.image}" alt="">
-  <h2 class="mt-5 text-center">Phone Name: ${info.slug}</h2>
-  <h3 class="text-center">Phone Brand Name: ${info.brand}</h3>
-  <h3 class="text-center">Features: ${info.mainFeatures}</h3>
-   <h3 class="text-center">Phone Released: ${info.releaseDate}</h3>
-  </div>
 
-  `
-};
+const setDetails = (detailsId) => {
+  console.log(detailsId)
+  const phoneDetails = document.getElementById("phone-details")
+  phoneDetails.textContent = "";
+  const div = document.createElement("div")
+  div.classList.add("col")
+  div.innerHTML = `
+  <div class="card p-3 shadow-lg">
+   <img width="100px"  src="${detailsId.image}" class="card-img-top img-fluid" alt="...">
+   <div class="card-body">
+     <h5 class="card-title">Brand:${detailsId.brand}</h5>
+     <p class="card-text"> Model: ${detailsId.mainFeatures.storage}</p>
+     <p class="card-text"> display size: ${detailsId.mainFeatures.displaySize}</p>
+     <p class="card-text"> Memory Card: ${detailsId.mainFeatures.memory}</p>
+     <p class="card-text"> Others: ${detailsId.others.Bluetooth}</p>
+     <p class="card-text"> USB: ${detailsId.others.USB}</p>
+     <p class="card-text"> Release date: ${detailsId.releaseDate?detailsId.releaseDate:"Not found 404"}</p>
+    </div>
+  `;
+  phoneDetails.appendChild(div)
+}
+
+
+
+
+
+/* =============================old code============================  */
+
+
+
+
+
+
+/* // const allPhones = () => {
+//   document.getElementById("phone-container").innerHTML = "";
+//   // document.getElementById("spinner").style.display = "block";
+//     const searchValue = document.getElementById('search-box').value;
+
+//   const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
+
+// //   console.log(url);
+//   fetch(url)
+//   .then((res) => res.json())
+//   .then((data) => {
+//     // console.log(data.data == null)
+//     if(data.data.length == 0 ) {
+//       // document.getElementById("spinner").style.display = "block";
+//       alart("page not found")
+//     }
+//     else {
+//       showPhoneDetails(data.data.slice(0,20));
+//       // document.getElementById("spinner").style.display = "none";
+//     }
+//   });
+  
+      
+// };
+
+// update code */
 
